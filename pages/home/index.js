87975e7f0dfae85e4db79ee867adc609618a53d7
kaikdorvalo <kaikdorvalo@gmail.com>
2024-06-03 03:11:54 -0300

    upload de imagem de livro funcionando

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,11 +1,50 @@
+const imgWebHook = 'https://discord.com/api/webhooks/1247059657076248639/FM5omKmr4mDKqgk8Nfs3XfMtqkNNY1roZvvWx598U3qWOuWXTcfBQMPl5tJN6pUr8W2K'
+
 document.addEventListener("DOMContentLoaded", () => {
     loadPage();
 })
 
 function loadPage() {
     loadCarousel();
+    publiImgListener()
 }
 
+function publiImgListener() {
+    const file = document.getElementById('dialog-publi-file');
+
+    file.addEventListener('change', async () => {
+        const selectedFile = file.files[0];
+
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        sendImageToWebhook(formData);
+    })
+}
+
+function sendImageToWebhook(file) {
+    var request = new XMLHttpRequest();
+
+    request.open(
+        "POST",
+        imgWebHook
+    )
+
+    request.send(file);
+
+    request.onload = alterImage
+}
+
+function alterImage() {
+    const obj = JSON.parse(this.responseText);
+    const imageUrl = obj.attachments[0].url;
+    console.log(imageUrl)
+
+    const div = document.getElementById('publi-img-view');
+    div.style.backgroundSize = 'cover'
+    div.style.opacity = '1'
+    div.style.backgroundImage = `url('${imageUrl}')`
+}
 
 function formatCEP(str) {
     var cep = str.replace(/\D/g, "");
